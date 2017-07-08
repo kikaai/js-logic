4067a14564f50873a7f828fd319c119d54b1efac
Karlyn Kaai <kikaai@hawaii.edu>
2017-07-07 21:49:38 -1000

    completed isEqual exercise

--- exercises.js
+++ exercises.js
@@ -119,8 +119,14 @@ console.log(bigBird("sup"));
  * If the strings are equal, the function will return the message "You look mahvelous!" Otherwise, return the message: "I don't know who you are anymore." 
  * Console.log your result.
 */
-
-
+function isEqual(first, second){
+	if(first === second){
+		return "You look mahvelous !"
+	}else{
+		return "I don't know who you are anymore."
+	}
+}
+console.log(isEqual("boom", "boom"));
 /*
  * #7
  * Function - notEqual
