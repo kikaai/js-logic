e7326f5118c1c9e1d0b98247f7ae8b5b2493aefb
Karlyn Kaai <kikaai@hawaii.edu>
2017-07-08 23:13:17 -1000

    completed exercise graduation

--- exercises.js
+++ exercises.js
@@ -244,8 +244,15 @@ console.log(daClub(1,0));
  * If EITHER the number value is greater than or equal to 120 or the boolean value is true, then the function will return the message: "Congratulations on a job well done." Otherwise, return the message: "See you in summer school."
  * Console.log your result.
 */ 
+function graduation(credits, thesis){
+	if((credits >= 120) || (thesis === true)){
 
-
+return "Congratulations on a job well done.";
+		}else{
+	return "see you in summer school.";
+}
+}
+console.log(graduation(121, false));
 
 /*
  * #13
