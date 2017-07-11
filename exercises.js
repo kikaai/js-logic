baffd35c4767b7132d7d243697232a973779917a
Karlyn Kaai <kikaai@hawaii.edu>
2017-07-10 20:15:35 -1000

    completed myFavFoods exercise

--- exercises.js
+++ exercises.js
@@ -349,6 +349,9 @@ for (var i = 0; i<Players.length; i++){
 */
   var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
 
+for (var i = 0; i<myFavFoods.length; i++){
+	console.log(myFavFoods[i]);
+}
 
 /*
  * #17
